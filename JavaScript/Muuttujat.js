abc86f232a1643aa41a2088c0e68f2ad7f520e4a
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-03 15:25:34 +0300

    Lisätty JavaScript- ja Python-koodiesimerkit.

---
+++ JavaScript/Muuttujat.js
@@ -0,0 +1,15 @@
+let omaNimi = "Jani";
+let kengännumero = 41;
+
+// muuttujan arvon muutos ja samalla tietotyypin vaihto
+kengännumero = "ABCD";
+console.log(kengännumero);
+
+
+
+// yhteenlaskua
+let m1 = 123
+let m2 = "987"
+
+summa = m2 + m1;
+console.log(summa);
---
+++ Python/Muuttujat.py
@@ -0,0 +1,6 @@
+oma_nimi = "Jani"
+kengännumero = 41
+
+# muuttujan arvon muutos ja samalla tietotyypin vaihto
+kengännumero = "ABCD"
+print("Toimii!")
