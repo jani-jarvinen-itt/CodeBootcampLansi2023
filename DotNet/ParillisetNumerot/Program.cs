ade194d9fd454baa67faf8b8706cf57e97f2fda4
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-08 11:33:26 +0300

    Lisätty C#-esimerkki parillisten numeroiden tulostamiseen kolmella eri tavalla.

---
+++ DotNet/ParillisetNumerot/Program.cs
@@ -0,0 +1,25 @@
+﻿// tapa 1
+for (int laskuri = 1; laskuri <= 20; laskuri++)
+{
+    if (laskuri % 2 == 0)
+    {
+        Console.WriteLine(laskuri);
+    }
+}
+
+Console.WriteLine("--------------");
+
+// tapa 2
+for (int laskuri = 2; laskuri <= 20; laskuri = laskuri + 2)
+{
+    Console.WriteLine(laskuri);
+}
+
+Console.WriteLine("--------------");
+
+// tapa 3
+int laskuri2 = 2;
+while (laskuri2 <= 20) {
+    Console.WriteLine(laskuri2);
+    laskuri2 = laskuri2 + 2;
+}
