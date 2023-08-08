915b6d0f1a937b26fab8a4259007b8122f04c2de
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-08 14:43:38 +0300

    Lisätty algoritimin dokumentaatio C#-koodiin.

--- DotNet/ArvaaLukuPeli/Program.cs
+++ DotNet/ArvaaLukuPeli/Program.cs
@@ -1,4 +1,16 @@
-﻿Random satunnainen = new();
+﻿/*
+Algoritmi:
+
+1. Tietokone arpoo numeron 1-20.
+2. Kysy pelaajalta luku.
+3. Tarkista pelaajan arvaus:
+   a. Jos oikein, pelaaja voittaa. Lopeta peli.
+   b. Jos oikea luku on pienempi, kerro se.
+   c. Jos oikea luku on suurempi, kerro se.
+4. Mene kohtaa 2, jos arvauskertoa (3 kpl) on vielä jäljellä.
+*/
+
+Random satunnainen = new();
 int oikea = satunnainen.Next(1, 21);
 // Console.WriteLine(oikea);
 
