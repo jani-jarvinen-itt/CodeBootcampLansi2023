7f6477af1a63c70d5dfecb3cd78183bbfaee4575
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-08 14:25:23 +0300

    Aloitettu koodaamaan C#:lla Arvaa luku -peliä.

---
+++ DotNet/ArvaaLukuPeli/Program.cs
@@ -0,0 +1,20 @@
+﻿Random satunnainen = new();
+int oikea = satunnainen.Next(1, 21);
+Console.WriteLine(oikea);
+
+Console.WriteLine("Anna arvauksesi välillä 1-20:");
+string syöte = Console.ReadLine();
+int arvaus = int.Parse(syöte);
+
+if (arvaus < oikea)
+{
+    Console.WriteLine("Oikea luku on suurempi!");
+}
+else if (arvaus > oikea)
+{
+    Console.WriteLine("Oikea luku on pienempi!");
+}
+else
+{
+    Console.WriteLine("Hienoa! Arvasit oikein ja voitit pelin!");
+}
