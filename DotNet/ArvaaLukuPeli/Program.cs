8fd15359d3982320856ac5f8e20c5f992d769eac
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-08 14:43:05 +0300

    Lisätty pelilogiikka, jossa pelaajalla on kolme arvauskertaa.

--- DotNet/ArvaaLukuPeli/Program.cs
+++ DotNet/ArvaaLukuPeli/Program.cs
@@ -1,20 +1,26 @@
 ﻿Random satunnainen = new();
 int oikea = satunnainen.Next(1, 21);
-Console.WriteLine(oikea);
+// Console.WriteLine(oikea);
 
-Console.WriteLine("Anna arvauksesi välillä 1-20:");
-string syöte = Console.ReadLine();
-int arvaus = int.Parse(syöte);
+for (int arvauskerta = 0; arvauskerta < 3; arvauskerta++)
+{
+    Console.WriteLine("Anna arvauksesi välillä 1-20:");
+    string syöte = Console.ReadLine();
+    int arvaus = int.Parse(syöte);
 
-if (arvaus < oikea)
-{
-    Console.WriteLine("Oikea luku on suurempi!");
+    if (arvaus < oikea)
+    {
+        Console.WriteLine("Oikea luku on suurempi!");
+    }
+    else if (arvaus > oikea)
+    {
+        Console.WriteLine("Oikea luku on pienempi!");
+    }
+    else
+    {
+        Console.WriteLine("Hienoa! Arvasit oikein ja voitit pelin!");
+        break;
+    }
 }
-else if (arvaus > oikea)
-{
-    Console.WriteLine("Oikea luku on pienempi!");
-}
-else
-{
-    Console.WriteLine("Hienoa! Arvasit oikein ja voitit pelin!");
-}
+
+Console.WriteLine("Peli on päättynyt.");
