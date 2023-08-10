88335ab272ceb2a8be5189ac0c3d9e066e5d806f
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-10 15:55:54 +0300

    Lisätty C#-kielinen esimerkki graafisesta työpöytäkäyttöliittymästä WinForms-tekniikalla.

---
+++ DotNet/WindowsFormsDemo/WindowsFormsDemo/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+
+            int sum = a + b;
+            MessageBox.Show("Summa on: " + sum);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+        }
+    }
+}
