0bf8af0ee48c0747f84197feee169d3e9f0fd82a
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-11 13:08:25 +0300

    Lisätty C#-esimerkki lukualueista int- ja byte-tietotyypeillä.

---
+++ DotNet/Lukualueet/Lukualueet/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// summan laskenta int-tietotyypillä
+int a = 100;
+int b = 200;
+
+int summa = a + b;                            // tulos: 300
+Console.WriteLine($"Int-tietotyypillä summa on {summa}.");
+
+// ***************************************
+
+// summan laskenta byte-tietotyypillä
+byte c = 100;
+byte d = 200;
+
+byte summa2 = (byte)(c + d);                  // tulos: 44!
+Console.WriteLine($"Byte-tietotyypillä summa on {summa2}.");
