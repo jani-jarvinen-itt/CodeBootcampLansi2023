254a3c83f38482b2f7b55ea7eed0a59a5892a0cb
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-11 13:36:21 +0300

    Lisätty esimerkki lukujen laskennasta silmukassa. Lisätty myös esimerkit suoritusajoista Intel-suorittimella.

--- DotNet/Merkkijonot/Merkkijonot/Program.cs
+++ DotNet/Merkkijonot/Merkkijonot/Program.cs
@@ -1,11 +1,27 @@
-﻿
+﻿/*
+ - Intel Core i9-9900 CPU @ 3.10GHz
+ - 500k kertaa: 38,5 sekuntia
+ - 600k kertaa: 58 sekuntia
+*/
+
 DateTime alku = DateTime.Now;
 
 string s = "";
-for (int i = 0; i < 500_000; i++)
+for (int i = 0; i < 600_000; i++)
 {
     s += "A";
 }
+
+/*
+int tulos = 0;
+for (int i = 0; i < 500_000; i++)
+{
+    // tulos = tulos + 1;
+    // tulos += 1;
+    tulos++;
+}
+Console.WriteLine(tulos);
+*/
 
 DateTime loppu = DateTime.Now;
 
