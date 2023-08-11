0a5bfb213f28239f5e14c715edd914712d65ded0
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-11 13:08:48 +0300

    Lisätty merkkijonojen rakentamisen esimerkki C#:lla.

---
+++ DotNet/Merkkijonot/Merkkijonot/Program.cs
@@ -0,0 +1,13 @@
+﻿
+DateTime alku = DateTime.Now;
+
+string s = "";
+for (int i = 0; i < 500_000; i++)
+{
+    s += "A";
+}
+
+DateTime loppu = DateTime.Now;
+
+TimeSpan kesto = loppu - alku;
+Console.WriteLine("Silmukan kesto: " + kesto.TotalSeconds);
