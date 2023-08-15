c3745ceca25ff9d48df805ef264b83c3e356de3f
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-15 13:57:20 +0300

    Aloitettu koodaamaan C#-sovellusta joka keskustelee SQL Server -tietokannan "Northwind" kanssa. Käytössä on ADO.NET-tekniikka.

---
+++ DotNet/SqlTietokantaDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+string yhteysmerkkijono = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;Encrypt=no;";
+SqlConnection yhteys = new(yhteysmerkkijono);
+yhteys.Open();
+Console.WriteLine("Tietokantayhteys avattu!");
+
+string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+SqlCommand komento = new(sql, yhteys);
+
+SqlDataReader lukija = komento.ExecuteReader();
+while (lukija.Read())
+{
+    string yritys = lukija["CompanyName"].ToString();
+    Console.WriteLine(yritys);
+}
+
+lukija.Close();
+komento.Dispose();
+yhteys.Close();
+Console.WriteLine("Tietokantayhteys suljettu.");
