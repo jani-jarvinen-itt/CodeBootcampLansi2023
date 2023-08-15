8d184ee6d20a2f831eb33d719b4625e297c08893
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-15 15:51:00 +0300

    Lisätty "dotnet"-komento NuGet-paketin asentamiseksi sekä muutettu using-lauseen käyttöä.

--- DotNet/SqlTietokantaDemo/Program.cs
+++ DotNet/SqlTietokantaDemo/Program.cs
@@ -1,22 +1,18 @@
-﻿using Microsoft.Data.SqlClient;
+﻿// paketin asennus: dotnet add package Microsoft.Data.SqlClient
+using Microsoft.Data.SqlClient;
 
 string yhteysmerkkijono = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;Encrypt=no;";
-using (SqlConnection yhteys = new(yhteysmerkkijono))
-{
-    yhteys.Open();
-    Console.WriteLine("Tietokantayhteys avattu!");
+using SqlConnection yhteys = new(yhteysmerkkijono);
+yhteys.Open();
+Console.WriteLine("Tietokantayhteys avattu!");
 
-    string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
-    using (SqlCommand komento = new(sql, yhteys))
-    {
-        using (SqlDataReader lukija = komento.ExecuteReader())
-        {
-            while (lukija.Read())
-            {
-                string yritys = lukija["CompanyName"]?.ToString() ?? "";
-                Console.WriteLine(yritys);
-            }
-        }
-    }
+string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+using SqlCommand komento = new(sql, yhteys);
+using SqlDataReader lukija = komento.ExecuteReader();
+while (lukija.Read())
+{
+    string yritys = lukija["CompanyName"]?.ToString() ?? "";
+    Console.WriteLine(yritys);
 }
+
 Console.WriteLine("Tietokantayhteys suljettu.");
