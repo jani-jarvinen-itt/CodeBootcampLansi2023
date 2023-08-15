2db9e39b6f828953574a2f4e0a49e26200f80e52
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-15 14:46:19 +0300

    Lisätty perinteinen resurssien suojaus try-finally -komennoilla.

--- DotNet/SqlTietokantaDemo/Program.cs
+++ DotNet/SqlTietokantaDemo/Program.cs
@@ -2,20 +2,36 @@
 
 string yhteysmerkkijono = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;Encrypt=no;";
 SqlConnection yhteys = new(yhteysmerkkijono);
-yhteys.Open();
-Console.WriteLine("Tietokantayhteys avattu!");
-
-string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
-SqlCommand komento = new(sql, yhteys);
-
-SqlDataReader lukija = komento.ExecuteReader();
-while (lukija.Read())
+try
 {
-    string yritys = lukija["CompanyName"].ToString();
-    Console.WriteLine(yritys);
-}
+    yhteys.Open();
+    Console.WriteLine("Tietokantayhteys avattu!");
 
-lukija.Close();
-komento.Dispose();
-yhteys.Close();
-Console.WriteLine("Tietokantayhteys suljettu.");
+    string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+    SqlCommand komento = new(sql, yhteys);
+    try
+    {
+        SqlDataReader lukija = komento.ExecuteReader();
+        try
+        {
+            while (lukija.Read())
+            {
+                string yritys = lukija["CompanyName"]?.ToString() ?? "";
+                Console.WriteLine(yritys);
+            }
+        }
+        finally
+        {
+            lukija.Close();
+        }
+    }
+    finally
+    {
+        komento.Dispose();
+    }
+}
+finally
+{
+    yhteys.Close();
+    Console.WriteLine("Tietokantayhteys suljettu.");
+}
