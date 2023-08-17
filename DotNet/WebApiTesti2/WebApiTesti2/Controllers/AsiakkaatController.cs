9df219f0fbc3b94bc46c83b928de1eda69bd578f
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-17 15:56:43 +0300

    Lisätty asiakkaat-kontrolleri joka käyttää Entity Framework -tekniikkaa SQL Server -tietokannan käsittelyyn.

---
+++ DotNet/WebApiTesti2/WebApiTesti2/Controllers/AsiakkaatController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTesti2.Models;
+
+namespace WebApiTesti2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AsiakkaatController : ControllerBase
+    {
+        [HttpGet]
+        public List<Customer> HaeSuomalaiset()
+        {
+            NorthwindContext konteksti = new();
+
+            // LINQ-kysely
+            var suomalaiset = from c in konteksti.Customers
+                              where c.Country == "Finland"
+                              select c;
+
+            return suomalaiset.ToList();
+        }
+    }
+}
