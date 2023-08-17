af24b9f807d7b18bebce9ee73cd4099ac5bdb980
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-17 14:56:56 +0300

    Lisätty C#-kielinen API-kontrolleri.

---
+++ DotNet/WebApiTesti2/WebApiTesti2/Controllers/EkaTestiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTesti2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EkaTestiController : ControllerBase
+    {
+        public string SanoMoi()
+        {
+            return "Moi maailma!";
+        }
+    }
+}
