eaec3b7c12f0a4588f89c22ac8cd21a21b1164e3
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-17 13:41:45 +0300

    Lisätty kaksi ASP.NET Web API -sovellusta, toinen komentorivin kautta ja toinen Visual Studion kautta perustettuna.

--- DotNet/EntityFrameworkTesti/Program.cs
+++ DotNet/EntityFrameworkTesti/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+dotnet add package Microsoft.EntityFrameworkCore
+
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+
+dotnet add package Microsoft.EntityFrameworkCore.Design
+
+dotnet ef dbcontext scaffold "Server=localhost\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;Encrypt=no;" Microsoft.EntityFrameworkCore.SqlServer -o Models
+*/
+
+using EntityFrameworkTesti.Models;
+
+NorthwindContext konteksti = new();
+
+// LINQ-kysely
+var suomalaiset = from c in konteksti.Customers
+                  where c.Country == "Finland"
+                  select c;
+
+foreach (var suomalainen in suomalaiset)
+{
+    Console.WriteLine(suomalainen.CompanyName);
+}
