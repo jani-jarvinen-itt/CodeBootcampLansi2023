6f6bc4dee2a6cc737b136db25379e4c2cad27acc
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-18 10:33:11 +0300

    Lisätty yksinkertainen C#-esimerkki LINQ-kielen kyselystä.

---
+++ DotNet/LinqDemo/LinqDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// lukutaulukko (array)
+int[] luvut = { 5, 11, 8, 4, 12, 6, 3, 7, 1 };
+
+// tulosta viittä suuremmat luvut numerojärjestyksessä
+var viittäSuuremmat = from l in luvut
+                      where l > 5
+                      orderby l
+                      select l;
+
+foreach (int luku in viittäSuuremmat)
+{
+    Console.WriteLine(luku);
+}
