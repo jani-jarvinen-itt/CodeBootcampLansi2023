a091a99b4f839857bb843bc5504f58139d454070
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-21 13:31:01 +0300

    Korjattu React-komponentti siten, ettei se enää jää silmukkaan hakemaan tietoja fetch-kutsulla. Korjattu myös käyttäjänimien visualisointi.

--- React/my-app/src/Käyttäjät.js
+++ React/my-app/src/Käyttäjät.js
@@ -4,19 +4,22 @@ export default function Käyttäjät() {
     const [käyttäjäLista, setKäyttäjäLista] = useState([]);
 
     console.log("Käyttäjät-komponentti: ennen useEffect-kutsua.");
-    useEffect( () => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(json => {
-                console.log(json);
-                setKäyttäjäLista(json);
-            });
+    useEffect(() => {
+        if (käyttäjäLista.length === 0) {
+            fetch('https://jsonplaceholder.typicode.com/users')
+                .then(response => response.json())
+                .then(json => {
+                    console.log(json);
+                    setKäyttäjäLista(json);
+                });
+        }
     });
 
     console.log("Käyttäjät-komponentti: render-vaihe.");
     let lista = [];
-    for (const käyttäjä in käyttäjäLista) {
-        lista.push(<p>{käyttäjä.name}</p>);
+    for (const indeksi in käyttäjäLista) {
+        // console.log(indeksi);
+        lista.push(<p>{käyttäjäLista[indeksi].name}</p>);
     }
 
     return <div>
