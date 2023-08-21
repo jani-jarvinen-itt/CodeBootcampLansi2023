4191023a7a3e7a426c1b35a535117f64c25dac42
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-21 11:58:37 +0300

    Aloitettu lisäämään tilanhallintaa (state) React-komponenttiin.

--- React/my-app/src/Käyttäjät.js
+++ React/my-app/src/Käyttäjät.js
@@ -1,4 +1,15 @@
-export default function Käyttäjät()
-{
-    return <h1>Moi! Olen Käyttäjät-komponentti.</h1>
+import { useState } from 'react';
+
+export default function Käyttäjät() {
+    const [käyttäjäLista, setKäyttäjäLista] = useState([]);
+
+    let lista = [];
+    for (const käyttäjä in käyttäjäLista) {
+        lista.push(<p>{käyttäjä.name}</p>);
+    }
+
+    return <div>
+        <h1>Moi! Olen Käyttäjät-komponentti.</h1>
+        {lista}
+    </div>
 }
