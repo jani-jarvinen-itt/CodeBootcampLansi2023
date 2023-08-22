a9e4b5af955cfe13e05a67c70bf9d1e8b9daba35
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-22 11:49:30 +0300

    Lisätty Python-koodia yhden rivin lisäämiseksi Asiakkat-tietokantatauluun.

--- Python/SqliteTietokantaDemo.py
+++ Python/SqliteTietokantaDemo.py
@@ -4,10 +4,18 @@ yhteys = sqlite3.connect("Asiakkaat.db")
 print("Tietokantayhteys avattu.")
 
 kursori = yhteys.cursor()
-kursori.execute("CREATE TABLE asiakkaat(asiakasid, nimi, maa)")
-print("Taulu 'asiakkaat' lisätty.")
+# kursori.execute("CREATE TABLE asiakkaat(asiakasid, nimi, maa)")
+# print("Taulu 'asiakkaat' lisätty.")
 
-# ???
+
+asiakasid = "1000"
+nimi = "Wilman Kala"
+maa = "Suomi"
+# kursori.execute("INSERT INTO asiakkaat VALUES ('" + asiakasid + "', '" + nimi + "', '" + maa + "')")
+kursori.execute(f"INSERT INTO asiakkaat VALUES ('{asiakasid}', '{nimi}', '{maa}')")
+print("Uusi asiakas lisätty tauluun.")
+
+
 
 yhteys.commit()
 yhteys.close()
