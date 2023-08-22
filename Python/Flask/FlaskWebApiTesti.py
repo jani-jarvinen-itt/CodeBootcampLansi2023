f663fb45604106d10ff1743d9f84e7858d5f507d
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-22 14:46:54 +0300

    Lisätty Web API -toiminto, joka lukee asiakastietoja SQLite-tietokannasta ja palauttaa tiedot JSON-muodossa.

--- Python/Flask/FlaskWebApiTesti.py
+++ Python/Flask/FlaskWebApiTesti.py
@@ -2,8 +2,19 @@
 # flask --app .\FlaskWebApiTesti.py run
 
 from flask import Flask
+import sqlite3
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def moi_maailma():
+    return "<p>Moikka maailma, terveisiä Code Bootcampiltä!</p>"
+
+@app.route("/api/asiakkaat")
+def hae_asiakkaat():
+    yhteys = sqlite3.connect("..\\Asiakkaat.db")
+    kursori = yhteys.cursor()
+    vastaus = kursori.execute("SELECT * FROM asiakkaat")
+    tulos = vastaus.fetchall()
+    yhteys.close()
+    return tulos
