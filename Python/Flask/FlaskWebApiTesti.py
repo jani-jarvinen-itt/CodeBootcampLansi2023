b0ff5e4febe0197b5e65802875be1491fc71ce69
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-22 14:01:44 +0300

    Aloitettu Flask-kehitys Pythonilla.

---
+++ Python/Flask/FlaskWebApiTesti.py
@@ -0,0 +1,9 @@
+# Sovelluksen käynnistykomento:
+# flask --app .\FlaskWebApiTesti.py run
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
