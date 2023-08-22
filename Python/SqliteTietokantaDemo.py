aa83ca05cdb40f232a65dfbbf5210e5eb7c51a74
Jani Järvinen <jani.jarvinen@intertechno.org>
2023-08-22 11:57:31 +0300

    Lisätty koodi joka lukee CSV-aineiston ja tallentaa sen SQLite-tietokantaan.

--- Python/SqliteTietokantaDemo.py
+++ Python/SqliteTietokantaDemo.py
@@ -7,16 +7,23 @@ kursori = yhteys.cursor()
 # kursori.execute("CREATE TABLE asiakkaat(asiakasid, nimi, maa)")
 # print("Taulu 'asiakkaat' lisätty.")
 
+tiedosto = open("Asiakkaat.csv", "r", encoding="utf8")
+print("CSV-tiedosto avattu.")
 
-asiakasid = "1000"
-nimi = "Wilman Kala"
-maa = "Suomi"
-# kursori.execute("INSERT INTO asiakkaat VALUES ('" + asiakasid + "', '" + nimi + "', '" + maa + "')")
-kursori.execute(f"INSERT INTO asiakkaat VALUES ('{asiakasid}', '{nimi}', '{maa}')")
-print("Uusi asiakas lisätty tauluun.")
+rivinumero = 1
+for rivi in tiedosto:
+  if rivinumero > 1:
+    osat = rivi.split(";")
 
+    asiakasid = osat[0]
+    nimi = osat[1]
+    maa = osat[2].strip()
+    kursori.execute(f"INSERT INTO asiakkaat VALUES ('{asiakasid}', '{nimi}', '{maa}')")
+    print(f"Uusi asiakas lisätty tauluun: {nimi}.")
 
+  rivinumero = rivinumero + 1
 
+tiedosto.close()
 yhteys.commit()
 yhteys.close()
-print("Tallennus kantaan tehty.")
+print("Tallennus kantaan tehty ja resurssit suljettu.")
